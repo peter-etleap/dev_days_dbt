1809cd970cc9b36a501f7772aab62d025df64ff5
Christian Romming <christian@etleap.com>
2024-12-01 23:00:59 -0800

    Removed staging table indirection.

--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
 SELECT p.user_id, SUM(i.price) AS spend
-  FROM {{ref('stg_mysql_purchase')}} p 
-    INNER JOIN {{ref('stg_mysql_line_item')}} li ON li.purchase_id = p.id
-    INNER JOIN {{ref('stg_mysql_item')}} i ON i.item_id = li.item_id
+  FROM {{source('DBTDEMO', 'PURCHASE')}} p
+    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
+    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} i ON i.item_id = li.item_id
   GROUP BY p.user_id
