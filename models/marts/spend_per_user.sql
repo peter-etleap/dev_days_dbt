b5c2a50f251546bfadf5a52b14ace29ba0e31314
Christian Romming <christian@etleap.com>
2024-12-01 23:22:12 -0800

    Fixed table name,.

--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
 SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('DBTDEMO', 'PURCHASE')}} p
     INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
-    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} i ON i.item_id = li.item_id
+    INNER JOIN {{source('DBTDEMO', 'ITEM')}} i ON i.item_id = li.item_id
   GROUP BY p.user_id
