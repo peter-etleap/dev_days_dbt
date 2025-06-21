bf492ac6b3d76b3e067395f9f7496a65bcf9c3b8
Peter Huang <peter@etleap.com>
2025-06-20 17:00:19 -0700

    test commit (#1)

--- models/marts/spend_by_login.sql
+++ models/marts/spend_by_login.sql
@@ -5,3 +5,5 @@ FROM {{ref('spend_per_user')}} s
   INNER JOIN {{ref('logins_by_user')}} l ON l.user_id = s.user_id
 GROUP BY s.user_id
 ORDER BY spend_by_login desc
+
+-- test
---
+++ models/trade_anomaly/test_subdirectory/int_watchlist_anomaly_2.sql
@@ -0,0 +1,20 @@
+{{ config(
+    pre_hook=[
+        "ALTER ICEBERG TABLE PUBLIC.\"TRADE_ANOMALY_rCnLfXHS\" REFRESH"
+    ]
+) }}
+
+select
+  account.zenith_account_id__c as account_id,
+  account.name as account_name,
+  anomaly.symbol__c as symbol,
+  anomaly.severity__c as anomaly_severity,
+  anomaly.reason__c as anomaly_reason,
+  anomaly.created_date as anomaly_create_date,
+  watchlist.watchlist_date,
+  watchlist.watch_reason
+from {{ source('PUBLIC', 'TRADE_ANOMALY') }} anomaly
+join {{ source('PUBLIC', 'SALESFORCE_ACCOUNT' )}} account
+  on anomaly.account__c = account.id
+join {{ source('PUBLIC', 'WATCHLIST') }} watchlist
+  on watchlist.entity_name = account.name
