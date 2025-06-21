2f916d178847c38854d99f4cddae6a7e8cbaba70
Peter Huang <peter@etleap.com>
2025-06-20 17:17:22 -0700

    add missing model paths

---
+++ cow/logins_by_user.sql
@@ -0,0 +1,4 @@
+SELECT u.id AS user_id, COUNT(e.external_id) AS logins 
+FROM {{source('DBTDEMO', 'USER_INFO')}} u, {{source('DBTDEMO', 'WEB_EVENTS')}} e
+WHERE u.external_id = e.external_id
+GROUP BY u.id
