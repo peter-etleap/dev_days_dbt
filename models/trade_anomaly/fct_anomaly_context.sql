fe6d637241c52c4295a2b9155dc2b9b5c888210c
Tester Dude (tester@etleap.com) <tester@etleap.com>
2025-07-23 17:21:02 +0000

    Updated fct_anomaly_context.

--- models/trade_anomaly/fct_anomaly_context.sql
+++ models/trade_anomaly/fct_anomaly_context.sql
@@ -13,4 +13,4 @@ left join {{ ref('int_watchlist_anomaly') }} watchlist_anomaly
   on symbol_order.account_id = watchlist_anomaly.account_id
   and symbol_order.symbol = watchlist_anomaly.symbol
   and watchlist_anomaly.anomaly_create_date > dateadd(minute, -5, current_timestamp())
-  -- test
+  -- tests
